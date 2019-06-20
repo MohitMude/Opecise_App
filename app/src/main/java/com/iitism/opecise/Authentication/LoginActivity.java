d342064bc6de113e9863e87b6148f9b901c90ab3
MohitMude <mohitmude19@gmail.com>
2019-06-20 19:55:47 +0530

    login page

---
+++ app/src/main/java/com/iitism/opecise/Authentication/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.iitism.opecise.Authentication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.iitism.opecise.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputPassword;
+    private Button btnLogin;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        textInputEmail=(TextInputLayout)findViewById(R.id.Edt_txt_email);
+        textInputPassword=(TextInputLayout)findViewById(R.id.Edt_txt_password);
+        btnLogin=(Button)findViewById(R.id.btn_login);
+    }
+
+
+
+    private  boolean validateEmail()
+    {
+        String email=textInputEmail.getEditText().getText().toString().trim();
+        if(email.isEmpty())
+        {
+            textInputEmail.setError("This field cannot be empty");
+            return false;
+        }
+        else{
+            textInputEmail.setError(null);
+            return true;
+        }
+    }
+    private  boolean validatePassword()
+    {
+        String password=textInputPassword.getEditText().getText().toString().trim();
+        if(password.isEmpty())
+        {
+            textInputPassword.setError("This field cannot be empty");
+            return false;
+        }
+        else{
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btnLogin)
+        {
+            if(!validateEmail() | !validatePassword()){
+                return;
+            }
+            Toast.makeText(this, "Working Fine", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
