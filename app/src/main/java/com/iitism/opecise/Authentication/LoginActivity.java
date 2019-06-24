75fc98e8957a7db1445531fc04a3fc9a337750c3
MohitMude <mohitmude19@gmail.com>
2019-06-24 08:46:42 +0530

    signUp page

--- app/src/main/java/com/iitism/opecise/Authentication/LoginActivity.java
+++ app/src/main/java/com/iitism/opecise/Authentication/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.iitism.opecise.Authentication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.iitism.opecise.R;
@@ -14,6 +16,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputPassword;
     private Button btnLogin;
+    private TextView newusersignup;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         textInputEmail=(TextInputLayout)findViewById(R.id.Edt_txt_email);
         textInputPassword=(TextInputLayout)findViewById(R.id.Edt_txt_password);
         btnLogin=(Button)findViewById(R.id.btn_login);
+        newusersignup=(TextView)findViewById(R.id.Txt_view_new_user);
+
+        newusersignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
@@ -61,6 +73,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
             Toast.makeText(this, "Working Fine", Toast.LENGTH_SHORT).show();
         }
+
+
     }
 
 
